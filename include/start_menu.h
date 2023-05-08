c2df36b1df01ca40ab9b500eddc961e758049410
Iceman8911 <wuchijss3@gmail.com>
2023-05-08 18:34:09 +0100

    Update start_menu.h

--- include/start_menu.h
+++ include/start_menu.h
@@ -6,7 +6,7 @@
 #define ROUND_UP_DIVISION_RESULT(number, divisor) (number % divisor == 0 ? number / divisor : (number + (divisor - (number % divisor))) / divisor)
 
 #define NUM_OF_START_MENUS                ROUND_UP_DIVISION_RESULT(FINAL_START_MENU_OPTION, NUM_OF_START_MENU_OPTIONS)     // Self Explanatory
-#define NUM_OF_START_MENU_OPTIONS         2                                                                                // Max number of options per menu. (Best to leave it as 8)
+#define NUM_OF_START_MENU_OPTIONS         8                                                                                // Max number of options per menu. (Best to leave it as 8)
 #define FINAL_START_MENU_OPTION           MENU_ACTION_EXIT                                                                 // Set this to the last startmenu option you want to print out
 #define IS_MULTI_START_MENU_ENABLED       FlagGet(FLAG_IS_MULTI_START_MENU_ENABLED)                                        // Self Explanatory
 
